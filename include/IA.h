a1f83481c5dd56112324e6b212c18ccd2bb62bb7
ynsamzni <ynsamzni@users.noreply.github.com>
2018-03-18 23:52:12 +0100

    Ajout des signatures liées à l'IA qui sont manquantes

--- include/IA.h
+++ include/IA.h
@@ -5,15 +5,22 @@
 #include "constantes.h"
 
 void initIA(StructJeu *jeu);
-
 void deplacerIA(StructJeu *jeu);
-
-void randomMove(StructJeu *jeu, int indiceJoueur, Direction direction, int situation, int faceAuMur);
-
-void debugRandomMove(int indiceJoueur, Direction direction, int situation, int faceAuMur);
-
+int calculerItineraires(int indiceJoueur, Coordonnes itineraire[300][300], int nbItineraire, int nbDeplacement, StructJeu *jeu);
+void suivreItineraire(int indiceJoueur, StructJeu *jeu);
+void comparerItineraires(int x, int y, Coordonnes itineraire[300][300], int nbTotalItineraire, StructJeu *jeu, Coordonnes casesDangereuses[148]);
+int comparerItineraireEloignementDangerosite(Coordonnes itineraire[300], Coordonnes casesDangereuses[148], int *longueurMeilleurItineraire);
+int comparerItineraireRapprochementTarget(int x, int y, Coordonnes itineraire[300], Coordonnes casesDangereuses[148], int *longueurMeilleurItineraire);
+int longueurItineraire(Coordonnes itineraire[300]);
+int rechercherItineraire(int x, int y, Coordonnes itineraire[300]);
+void copierItineraire(Coordonnes itineraireSrc[300], Coordonnes itineraireDest[300], int longueurACopier);
+void supprimerDeplacementItineraire(int deplacementASupprimer, Coordonnes itineraire[300]);
+void calculerCasesDangereuses(Coordonnes casesDangereuses[148], StructJeu *jeu);
+int coordonneesDangereuses(int x, int y, Coordonnes casesDangereuses[148]);
+int itineraireDangereux(Coordonnes itineraire[300], Coordonnes casesDangereuses[148]);
 int ennemiDansAxe(int indiceJoueur, StructJeu *jeu);
-
+void modifierCoordonnees(Coordonnes *coordonneesAModifier, int x, int y);
+void copierCoordonnees(Coordonnes *coordonneesSrc, Coordonnes *coordonneesDest);
 
 
 #endif // IA_H_INCLUDED
