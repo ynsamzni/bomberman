4bb1327e9abe7ec2f93646443aa8f979610b1d87
Ronan <rlv78@hotmail.fr>
2018-04-16 15:39:14 +0200

    Modification de la map

--- include/constantes.h
+++ include/constantes.h
@@ -15,7 +15,7 @@
 #define LONGUEUR_EXPLOSION_BOMBE 3
 #define NB_CASES_AFFECTEES_EXPLOSIONS_BOMBES (LONGUEUR_EXPLOSION_BOMBE * 4 + 1) * 4
 
-#define CHEMIN_D_ACCES_FICHIER_NIVEAU "assets/maps/map1.dat"
+#define CHEMIN_D_ACCES_FICHIER_NIVEAU "assets/maps/map.dat"
 #define CHEMIN_D_ACCES_FICHIER_COMPTES_JOUEURS "saves/comptes_joueurs.dat"
 #define CHEMIN_POLICE_ECRITURE_MONTSERRAT "assets/fonts/montserrat_regular.ttf"
 #define CHEMIN_POLICE_ECRITURE_MONTSERRAT_BOLD "assets/fonts/montserrat_bold.ttf"
